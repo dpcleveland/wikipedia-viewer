15a1beb45333eb5a0ceaa41064c67e187e93c29b
Daniel Cleveland <dpcleveland@gmail.com>
2016-10-02 21:58:22 -0500

    Testing expanding search

--- dynamic/custom.js
+++ dynamic/custom.js
@@ -1,4 +1,20 @@
-$(document).ready(function() {
+// Expanding Search Bar
+// http://codeconvey.com/expanding-search-bar-with-jquery/
 
-    
+$(document).ready(function(e) {
+    $(".SlideoutActive").toggle(function() {
+        $("ul li").each(function(index, element) {
+            $(this).animate({
+                "margin-left": index * 105
+            });
+            $(this).css("transition", "all .5s ease-in-out");
+        });
+    }, function() {
+        $(".SlideOutMenu ul li").each(function(index, element) {
+            $(this).animate({
+                "margin-left": 0
+            });
+        });
+    });
+    //
 });
